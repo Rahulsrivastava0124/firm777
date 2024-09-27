0b1a9f399af83e89a86d4af637114cbbba9459a9
Rahul srivastava <87309230+Rahulsrivastava0124@users.noreply.github.com>
2024-09-28 00:17:53 +0530

    "Fixed typo in submitForm function: changed `document.getElementById(SubmitIndex)` to `document.getElementById('SubmitIndex')`"

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const submitForm = (event) => {
     promocode:PromoCode,
   }).then(result => {
     console.log(result);
-    document.getElementById(SubmitIndex).innerHTML="Loding...";
+    document.getElementById('SubmitIndex').innerHTML="Loding...";
     return window.location.replace(url);
   }).catch((err) => {
     console.log(err)
